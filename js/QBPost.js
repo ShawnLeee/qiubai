92e87bacdbaf1e224a3092612404ede27a79c680
daniel <xiaoaoxtu@gmail.com>
2016-09-07 10:43:26 +0800

    add action bar

--- js/QBPost.js
+++ js/QBPost.js
@@ -11,7 +11,7 @@ function QBPost(post){
 
 function QBCell(qb_post)
 {
-	var cell = $("<div id=\"qb-cell\"><div class=\"qb-user clearfix\"> <a class=\"user-img\" href=\"#\"> <img src=\"\"> </a> <a class=\"user_name\" href=\"#\"> <h2></h2> </a> </div> <div class=\"qb-content\"> <p> </p> </div> <div class=\"qb-stats\"> <span class=\"stats-vote\"> <i class=\"number\">8888</i> 好笑 </span> <span class=\"stats-comment\"> <span class=\"dash\"> . </span> <a href=\"\"> <i class=\"number\">444</i> 评论 </a> </span></div></div>");
+	var cell = $("<div id=\"qb-cell\"><div class=\"qb-user clearfix\"> <a class=\"user-img\" href=\"#\"> <img src=\"\"> </a> <a class=\"user_name\" href=\"#\"> <h2></h2> </a> </div> <div class=\"qb-content\"> <p> </p> </div> <div class=\"qb-stats\"> <span class=\"stats-vote\"> <i class=\"number\">8888</i> 好笑 </span> <span class=\"stats-comment\"> <span class=\"dash\"> . </span> <a href=\"\"> <i class=\"number\">444</i> 评论 </a> </span></div><div class=\"action-bar clearfix\"> <ul class=\"clearfix\"> <li class=\"up\"> <a href=\"\"> <i></i> </a> </li> <li class=\"down\"> <a href=\"\"> <i></i> </a> </li> <li class=\"comment\"> <a href=\"\"> <i></i> </a> </li> </ul> </div> <div class=\"share-bar clearfix\"> <a class=\"share-wechat\"href=\"\"></a> <a class=\"share-qq\"href=\"\"></a> <a class=\"share-qzone\"href=\"\"></a> <a class=\"share-weibo\"href=\"\"></a> </div> <div class=\"clearfix\"></div></div>");
 	$(cell).find(".user-img").find("img").attr("src",qb_post.user_img);
 	$(cell).find(".user_name").find("h2").text(qb_post.user_name);
 	$(cell).find(".qb-content").find("p").text(qb_post.post_text);
