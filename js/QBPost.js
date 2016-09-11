59a1f04024e5a181d793e66b00116b4ad8e7a2f3
daniel <xiaoaoxtu@gmail.com>
2016-09-11 16:21:57 +0800

    add like feature

--- js/QBPost.js
+++ js/QBPost.js
@@ -28,7 +28,7 @@ function QBPost(post){
 
 function QBCell(qb_post)
 {
-	var cell = $("<div id=\"qb-cell\"><div class=\"qb-user clearfix\"> <a class=\"user-img\" href=\"#\"> <img src=\"\"> </a> <a class=\"user_name\" href=\"#\"> <h2></h2> </a> </div> <div class=\"qb-content\"> <p> </p> </div> <div class=\"qb-stats\"> <span class=\"stats-vote\"> <i class=\"number\">8888</i> 好笑 </span> <span class=\"stats-comment\"> <span class=\"dash\"> . </span> <a href=\"\"> <i class=\"number\">444</i> 评论 </a> </span></div><div class=\"action-bar clearfix\"> <ul class=\"clearfix\"> <li class=\"up\"> <a href=\"\"> <i></i> </a> </li> <li class=\"down\"> <a href=\"\"> <i></i> </a> </li> <li class=\"comment\"> <a href=\"\"> <i></i> </a> </li> </ul> </div> <div class=\"share-bar clearfix\"> <a class=\"share-wechat\"href=\"\"></a> <a class=\"share-qq\"href=\"\"></a> <a class=\"share-qzone\"href=\"\"></a> <a class=\"share-weibo\"href=\"\"></a> </div> <div class=\"clearfix\"></div></div>");
+	var cell = $("<div id=\"qb-cell\"><div class=\"qb-user clearfix\"> <a class=\"user-img\" href=\"#\"> <img src=\"\"> </a> <a class=\"user_name\" href=\"#\"> <h2></h2> </a> </div> <div class=\"qb-content\"> <p> </p> </div> <div class=\"qb-stats\"> <span class=\"stats-vote\"> <i class=\"number\">8888</i> 好笑 </span> <span class=\"stats-comment\"> <span class=\"dash\"> . </span> <a href=\"\"> <i class=\"number\">444</i> 评论 </a> </span></div><div class=\"action-bar clearfix\"> <ul class=\"clearfix\"> <li class=\"up\"> <a href=\"javascript:;\"> <i></i> </a> </li> <li class=\"down\"> <a href=\"javascript:;\"> <i></i> </a> </li> <li class=\"comment\"> <a href=\"javascript:;\"> <i></i> </a> </li> </ul> </div> <div class=\"share-bar clearfix\"> <a class=\"share-wechat\"href=\"\"></a> <a class=\"share-qq\"href=\"\"></a> <a class=\"share-qzone\"href=\"\"></a> <a class=\"share-weibo\"href=\"\"></a> </div> <div class=\"clearfix\"></div></div>");
 	$(cell).find(".user-img").find("img").attr("src",qb_post.user_img);
 	$(cell).find(".user_name").find("h2").text(qb_post.user_name);
 	$(cell).find(".qb-content").find("p").text(qb_post.post_text);
@@ -38,8 +38,32 @@ function QBCell(qb_post)
 	$(cell).find(".qb-content").find("p").click(function(){
 		var detailurl =  "article_detail.html?article_id=" + qb_post.post_id;
 		window.open(detailurl);
-	})
+	});
+
+	$(cell).find(".action-bar").find(".up").find("a").click(function(){
+		like_post(qb_post.post_id, 1, function(status){
+			if(status == 1)
+			{
+				$(cell).find(".stats-vote").find("i").text(qb_post.like_count+1);
+			}
+		});
+	});
+
+	$(cell).find(".action-bar").find(".down").find("a").click(function(){
+		like_post(qb_post.post_id, -1, function(status){
+			if(status == 1)
+			{
+				$(cell).find(".stats-vote").find("i").text(qb_post.like_count-1);
+			}
+		});
+	});
 	return cell;
+}
+function like_post(post_id, like_count,completion) 
+{
+	$.getJSON("http://52.43.221.136/api/v1.0/likeit/",{post_id: post_id,like: like_count},function(response){
+			completion(response.status);
+	})
 }
 function CommentCell(comment)
 {
